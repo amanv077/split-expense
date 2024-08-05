862146b45d1da32c92c5a8ed172ddd52865acbe2
amanv077 <amanverma4816@gmail.com>
2024-08-05 12:48:14 +0530

    n

--- src/components/Homepage/Home.js
+++ src/components/Homepage/Home.js
@@ -1,26 +1,26 @@
-import Box from "@mui/material/Box";
-import Navbar from "../Navbar/page";
-import CreateTrip from "../../Trip/CreateTrip";
-import NewExpense from "../../Trip/NewExpense";
-import Table from "../../Data/Table";
-import Overview from "../../OverviewDisplay/Overview";
+// import Box from "@mui/material/Box";
+// import Navbar from "../Navbar/page";
+// import CreateTrip from "../../Trip/CreateTrip";
+// import NewExpense from "../../Trip/NewExpense";
+// import Table from "../../Data/Table";
+// import Overview from "../../OverviewDisplay/Overview";
 
-const Home = (props) => {
-  const { allExpense, totalAmount, setTrips, trips, addNewExpense } = props;
+// const Home = (props) => {
+//   const { allExpense, totalAmount, setTrips, trips, addNewExpense } = props;
 
-  return (
-    <Box>
-      <Navbar allExpense={allExpense} totalAmount={totalAmount} />
-      <CreateTrip setTrips={setTrips} trips={trips} />
-      <NewExpense
-      // addNewExpense={addNewExpense}
-      // trips={trips}
-      // allExpense={allExpense}
-      />
-      <Table allExpense={allExpense} />
-      <Overview allExpense={allExpense} trip={trips[0]} />
-    </Box>
-  );
-};
+//   return (
+//     <Box>
+//       <Navbar allExpense={allExpense} totalAmount={totalAmount} />
+//       <CreateTrip setTrips={setTrips} trips={trips} />
+//       <NewExpense
+//       // addNewExpense={addNewExpense}
+//       // trips={trips}
+//       // allExpense={allExpense}
+//       />
+//       <Table allExpense={allExpense} />
+//       <Overview allExpense={allExpense} trip={trips[0]} />
+//     </Box>
+//   );
+// };
 
-export default Home;
+// export default Home;
